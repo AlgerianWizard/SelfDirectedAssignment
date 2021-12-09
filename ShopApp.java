f887c9c3a3496bcc997c908bef798beaaf176baa
AlgerianWizard <95402236+AlgerianWizard@users.noreply.github.com>
2021-12-09 19:51:36 +0000

    Streamlined some code, added new loop in main method for the system to keep going, still need to simplify other method similar to buyStock

--- ShopApp.java
+++ ShopApp.java
@@ -17,37 +17,36 @@ public class ShopApp
         itemArray[1] = new Product("Water", 5, 1.20);
         itemArray[2] = new Product("Soda", 10, 1.05);
         itemArray[3] = new Product("Juice", 5, 1.20);
-        itemArray[4] = new Product("Energy drink", 5, 1.5);
-
-        int selection = UI(); //Calls UI method and assigns an integer to selection
+        itemArray[4] = new Product("Energy drink", 5, 1.50);
+        UI(itemArray);
         
-        switch(selection)
+        String anotherTask = "Do you want to peform another task?(Y/N)";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(anotherTask);
+        char answer = sc.next().charAt(0);
+        
+
+        while (answer != 'Y' && answer != 'N')
         {
-            case 1: productList(itemArray);
-            break;  
-            case 2: buyStock(itemArray);
-            break;
-            case 3: sellStock(itemArray);
-            break;
-            case 4: setPrice(itemArray);
-            break;
-            case 5: TotalValue(itemArray);
-            break;
-            default:System.out.println("null");
+            System.out.println("Please enter a single character 'Y' or 'N'.");
+            answer = sc.next().charAt(0);
+        }
+        
+        while(answer == 'Y' || answer == 'y')
+        {
+            UI(itemArray);
+            System.out.println(anotherTask);
+            answer = sc.next().charAt(0);
         }
     }
 
 
-    public static int UI()
+    public static void UI(Product[] itemArray) //Returns selection from first menu 
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Menu:\n1)Product List\n2)Buy Stock \n3)Sell Stock\n4)Set Prices\n5)Total Stock Value");
         int choice = sc.nextInt();
-        while (choice < 1 || choice > 5)
-        {
-            System.out.println("Invalid input, enter number in range 1 - 5");
-            choice = sc.nextInt(); 
-        }
         
         int selection = 0;
         switch(choice)
@@ -62,10 +61,23 @@ public class ShopApp
             break;
             case 5: selection = 5;
             break;
-            default: System.out.println("null");
+            default: System.out.println("Invalid input, enter number in range 1 - 5"); choice = sc.nextInt(); //Switches have built in input validation 
         }
 
-        return selection;
+        switch(selection) //Menu with switch also passes arrays to all the functions
+        {
+            case 1: productList(itemArray); 
+            break;  
+            case 2: buyStock(itemArray);
+            break;
+            case 3: sellStock(itemArray);
+            break;
+            case 4: setPrice(itemArray);
+            break;
+            case 5: TotalValue(itemArray);
+            break;
+            default:System.out.println("null");
+        }
     }
 
     public static void productList(Product[] itemArrayin)
@@ -77,6 +89,7 @@ public class ShopApp
             System.out.println(listindex + "- " + itemArrayin[i].getName());
             listindex++;
         }
+
     }
     
     public static void buyStock(Product[] itemArrayin)
@@ -100,30 +113,11 @@ public class ShopApp
             System.out.println("Invalid input, please enter positive amount");
             stockAmount = sc.nextInt();
         }
-
-        String sB = "Stock bought: ";
-        String cSL = "Current stock level: ";
         
-        switch(choice)
-        {
-            case 1: System.out.println(itemArrayin[0].getName()); itemArrayin[0].buyStock(stockAmount); 
-            System.out.println(sB  + stockAmount); System.out.println(cSL + itemArrayin[0].getStockLevel());
-            break;
-            case 2: System.out.println(itemArrayin[1].getName()); itemArrayin[1].buyStock(stockAmount); 
-            System.out.println(sB + stockAmount); System.out.println(cSL + itemArrayin[1].getStockLevel());
-            break;
-            case 3: System.out.println(itemArrayin[2].getName()); itemArrayin[2].buyStock(stockAmount); 
-            System.out.println(sB + stockAmount); System.out.println(cSL + itemArrayin[2].getStockLevel());
-            break;
-            case 4: System.out.println(itemArrayin[3].getName()); itemArrayin[3].buyStock(stockAmount); 
-            System.out.println(sB + stockAmount); System.out.println(cSL + itemArrayin[3].getStockLevel());
-            break;
-            case 5: System.out.println(itemArrayin[4].getName()); itemArrayin[4].buyStock(stockAmount); 
-            System.out.println(sB + stockAmount); System.out.println(cSL + itemArrayin[4].getStockLevel());
-            break;
-            default: System.out.println("This should never print!");
-            // IDEA: MAYBE ADD BACK BUTTONS OR MAYBE LOOP CUZ THEY WANNA BUY MORE STOCK?
-        }
+        System.out.println(itemArrayin[choice-1].getName());
+        itemArrayin[choice-1].buyStock(stockAmount);
+        System.out.println("Stock bought: " + stockAmount);
+        System.out.println("Current stock level: " + itemArrayin[choice-1].getStockLevel());
     }
     
     public static void sellStock(Product[] itemArrayin)
@@ -162,6 +156,7 @@ public class ShopApp
             break;
             default: System.out.println("Invalid input, please enter a number in range 1-5.");
         }
+
     }
 
     public static void setPrice(Product[] itemArrayin)
@@ -195,6 +190,7 @@ public class ShopApp
             break;
             default: System.out.println("Invalid input, please enter a number in range 1-5."); //Input validation built into the switch
         }
+
     }
 
     public static void TotalValue(Product[] itemArrayin)
@@ -208,7 +204,34 @@ public class ShopApp
         }
         
         System.out.println("The total value of stock is: " + totalvalue);
+
     }
+
+    /*public static void backMenu()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you wish to perform another task?(Y/N)");
+        
+        char selection = sc.next().charAt(0);
+
+        if(selection == 'Y')
+        {
+            UI();
+        }
+        
+        else
+        ()
+        /*switch(selection)
+        {
+            case 'Y': UI();
+            break;
+            case 'N': System.out.println("End of Program.");
+            break;
+            default: System.out.println("Please enter 'Y' or 'N'."); selection = sc.next().charAt(0);
+        }
+    }*/
 }
+
+
 
 
